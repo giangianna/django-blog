210df1bb8498dad2a91a779652a4a2674db9c62a
Gian Gianna Pallan Pallas <gian.ule@gmail.com>
2024-09-16 23:29:56 +0700

    Adding Pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,11 +3,13 @@ from django.views import generic
 from .models import Post
 from .forms import CommentForm
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    # paginate_by = 3
 
 class PostDetail(generic.DetailView):
     model = Post
@@ -34,3 +36,21 @@ def post_detail(request, slug):
                                         'comments': comments,
                                         'new_comment': new_comment,
                                         'comment_form': comment_form})
+
+def post_list(request):
+    object_list = Post.objects.filter(status=1).order_by('-created_on')
+    paginator = Paginator(object_list, 3)
+  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+            # If page is not an integer deliver the first page
+        post_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        post_list = paginator.page(paginator.num_pages)
+    return render(request,
+                  'index.html',
+                  {'page': page,
+                   'post_list': post_list})
