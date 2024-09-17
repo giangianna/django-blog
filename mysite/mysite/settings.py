ad8ae55aada92bf9d5e617a8b45ecd644df796f2
Gian Gianna Pallan Pallas <gian.ule@gmail.com>
2024-09-18 00:47:16 +0700

    change db to postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'giangianna',
+        'PASSWORD': '000101',
+        'HOST': '141.11.25.251',
+        'PORT': '5432',
     }
 }
 
